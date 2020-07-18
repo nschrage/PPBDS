4417132b4288643e22279a75b9b0f95e3c1e4fae
David Kane <dave.kane@gmail.com>
2020-07-18 10:59:35 -0400

    document

--- common.R
+++ common.R
@@ -1,5 +1,11 @@
-# Usage: add `source("common.R")` to beginning of each chapter
+# This file is automatically sourced before each chapter is compiled. (Or is it
+# before each session, regardless of the number of chapters being put together?
+# That is, if chapter 4 changes the value for `option("digits")`, will the value
+# be reset for chapter 5? For now, I doubt we need to worry about that since
+# individual chapters rarely mess around with this stuff.)
 
-# Setting digits = 2 as default per chapter
+# This is caused by this setting in _bookdown.yml.
+
+# before_chapter_script: "common.R"
 
 options(digits = 2)
